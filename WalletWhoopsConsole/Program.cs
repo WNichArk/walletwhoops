361e9ecb51d120a73052f7f284441486698ccf7e
Will Nicholson <william.nicholson.dev@gmail.com>
2025-02-05 18:49:40 -0600

    init

---
+++ WalletWhoopsConsole/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Please enter drive letter for search, or leave blank to iterate all drives: ");
+var driveLetter = Console.ReadLine();
+if (string.IsNullOrWhiteSpace(driveLetter))
+{
+
+    var drives = DriveInfo.GetDrives();
+
+}
